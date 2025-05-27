97a4682ff6c1733eef3a3ff498da3bf7ddab8c42
berkekcay <134728762+berkekcay@users.noreply.github.com>
2025-05-27 13:54:50 +0300

    .

--- train_model.py
+++ train_model.py
@@ -7,8 +7,17 @@ import joblib
 
 # Burada beğeni sayısı rastgele oluşturuluyor, gerçek verilerle değiştir!
 data = pd.read_csv("data/features.csv")
-data['likes'] = np.random.randint(100, 1000, size=len(data))
+def simulate_likes(row):
+    score = 0
+    if 100 < row['brightness'] < 200:
+        score += 1
+    if row['sharpness'] > 300:
+        score += 2
+    if row['color_r'] > 100 or row['color_g'] > 100 or row['color_b'] > 100:
+        score += 1
+    return np.random.randint(100 + score * 50, 300 + score * 100)
 
+data['likes'] = data.apply(simulate_likes, axis=1)
 X = data[['brightness', 'sharpness', 'color_r', 'color_g', 'color_b']]
 y = data['likes']
 
